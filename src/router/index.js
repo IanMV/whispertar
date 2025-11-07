d868901670707b27d021446efc4e605d67244a96
Pauloartur-23 <pauloartur.ifc@gmail.com>
2025-11-06 23:24:15 -0300

    FEAT: Criação de login #6

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,21 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+    },
+    {
+      path: '/esqueci-senha',
+      name: 'forgot-password',
+      component: () => import('../views/ForgotPasswordView.vue'),
+    },
   ],
 })
 
